733b846b6e4804478cbf7d48593a233080cab4a9
vetocanti <vetocanti@gmail.com>
2024-07-10 14:48:45 -0400

    chore: Add periodic fetching of root endpoint
    ```

--- crud.py
+++ crud.py
@@ -1,5 +1,7 @@
 from sqlalchemy.orm import Session
 from . import models
+import time
+import requests
 
 def get_products_by_pages(db: Session, page: int, limit:int):
     if(page<=0):
@@ -24,3 +26,12 @@ def get_product_by_id(db: Session, id: int):
 
 def get_total_products_count(db: Session):
     return db.query(models.Product).count()
+
+def fetch_root_periodically():
+    while True:
+        try:
+            response = requests.get("http://localhost:8000/")
+            print(response.json())
+        except Exception as e:
+            print(f"Error fetching root: {e}")
+        time.sleep(180)  # Espera 3 minutos
--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from sqlalchemy.orm import Session
 from . import crud,models,schemas
 from .database import SessionLocal, engine
 from typing import List
+import threading
+
 
 
 models.Base.metadata.create_all(bind=engine)
@@ -29,48 +31,73 @@ def get_db():
     finally:
         db.close()
 
-
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
 @app.get("/products/", response_model=schemas.ProductResponseModel)
 def get_products(db: Session = Depends(get_db), page: int = 0, limit: int = 10):
-    products = crud.get_products_by_pages(db, page, limit)
-    total_amount = crud.get_total_products_count(db)
-    product_models = [schemas.ProductModel(id=product.id, name=product.name, price=product.price, discount=product.discount, url_image=product.url_image, category_id=product.category_id) for product in products]
-    return {
-        "page": page,
-        "limit": limit,
-        "total_amount": total_amount,
-        "records": product_models
-    }
+    try:
+        products = crud.get_products_by_pages(db, page, limit)
+        total_amount = crud.get_total_products_count(db)
+        product_models = [schemas.ProductModel(id=product.id, name=product.name, price=product.price, discount=product.discount, url_image=product.url_image, category_id=product.category_id) for product in products]
+        return {
+            "page": page,
+            "limit": limit,
+            "total_amount": total_amount,
+            "records": product_models
+        }
+    except Exception as e:
+        print(f"Error fetching products: {e}")
+        return {"error": "Error fetching products"}
 
 @app.get("/products_category/{category_id}", response_model=schemas.ProductResponseModel)
 def get_products_by_category(db: Session = Depends(get_db), category_id: int=1, page: int=0, limit: int=10):
-    products = crud.get_products_by_category(db, category_id, page, limit)
-    product_models = [schemas.ProductModel(id=product.id, name=product.name, price=product.price, discount=product.discount, url_image=product.url_image, category_id=product.category_id) for product in products]
+    try:
+        products = crud.get_products_by_category(db, category_id, page, limit)
+        product_models = [schemas.ProductModel(id=product.id, name=product.name, price=product.price, discount=product.discount, url_image=product.url_image, category_id=product.category_id) for product in products]
 
-    return {
-        "page": page,
-        "limit": limit,
-        "total_amount": len(products),
-        "records": product_models
-    }
+        return {
+            "page": page,
+            "limit": limit,
+            "total_amount": len(products),
+            "records": product_models
+        }
+    except Exception as e:
+        print(f"Error fetching products: {e}")
+        return {"error": "Error fetching products"}
 
 @app.get("/categories/", response_model=List[schemas.CategoryModel])
 def get_categories(db: Session = Depends(get_db)):
-    categories = crud.get_categories(db)
-    return categories
+    try:
+        categories = crud.get_categories(db)
+        return categories
+    except Exception as e:
+        print(f"Error fetching categories: {e}")
+        return {"error": "Error fetching categories"}
 
 @app.get("/products_name/{name}", response_model=schemas.ProductResponseModel)
 def get_product_by_name(name: str, db: Session = Depends(get_db), page: int = 0, limit: int = 10):
-    products = crud.get_product_by_name(db, name, page, limit)
-    products_model = [schemas.ProductModel(id=product.id, name=product.name, price=product.price, discount=product.discount, url_image=product.url_image, category_id=product.category_id) for product in products]
-    return {
-        "page": page,
-        "limit": limit,
-        "total_amount": len(products),
-        "records": products_model
-    }
+    try: 
+        products = crud.get_product_by_name(db, name, page, limit)
+        products_model = [schemas.ProductModel(id=product.id, name=product.name, price=product.price, discount=product.discount, url_image=product.url_image, category_id=product.category_id) for product in products]
+        return {
+            "page": page,
+            "limit": limit,
+            "total_amount": len(products),
+            "records": products_model
+        }
+    except Exception as e:
+        print(f"Error fetching products: {e}")
+        return {"error": "Error fetching products"}
 
 @app.get("/products_id/{id}", response_model=schemas.ProductModel)
 def get_product_by_id(id: int, db: Session = Depends(get_db)):
-    product = crud.get_product_by_id(db, id)
-    return product
+    try:
+        product = crud.get_product_by_id(db, id)
+        return product
+    except Exception as e:
+        print(f"Error fetching product: {e}")
+        return {"error": "Error fetching product"}
+thread = threading.Thread(target=crud.fetch_root_periodically)
+thread.daemon = True
+thread.start()
