67bc23da01a685d1910a2d594a019dc1edc49004
vetocanti <vetocanti@gmail.com>
2024-10-28 11:35:24 -0300

    Refactor database models for Product and Category
    - Update column types and lengths for name, discount, url_image, and category_id in the Product model
    - Update column type and length for name and id in the Category model

--- utils/models.py
+++ utils/models.py
@@ -7,16 +7,16 @@ class Product(database.Base):
     __tablename__ = "product"
 
     id = Column(Integer, primary_key=True, autoincrement=True)
-    name = Column(String, unique=True)
+    name = Column(String(255), unique=True)
     price = Column(Float)
-    discount = Column(Integer)
-    url_image = Column(String)
-    category_id = Column(Integer, ForeignKey("category.id"), name="category_id")
+    discount = Column(Integer(8))
+    url_image = Column(String(255))
+    category_id = Column(Integer(8), ForeignKey("category.id"), name="category_id")
     category = relationship("Category", back_populates="products")
 
 class Category(database.Base):
     __tablename__ = "category"
 
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    name = Column(String, unique=True)
+    id = Column(Integer(8), primary_key=True, autoincrement=True)
+    name = Column(String(255), unique=True)
     products = relationship("Product", back_populates="category")
